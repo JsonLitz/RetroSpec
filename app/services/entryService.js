97a76c8c2edbe655d1b46d208ca79d95c96ee97f
jsonlitz <jasondotlee@gmail.com>
2016-09-12 11:51:09 -0700

    add ajax calls for get entry and delete entry

--- app/services/entryService.js
+++ app/services/entryService.js
@@ -1,3 +1,44 @@
 var $ = require("jquery");
-var promise = require ("es6-promise");
-var resourceUrl = "http://localhost:7777/api/entries"
+var promise = require("es6-promise");
+var resourceUrl = "http://localhost:7777/api/entries";
+
+module.exports = {
+  addEntry: function (entry) {
+    var Promise = promise.Promise;
+    return new Promise(function (resolve, reject) {
+      $.ajax({
+        url:resourceUrl,
+        data: JSON.stringify(entry),
+        method: "POST",
+        dataType: "json",
+        contentType: "application/json",
+        success: resolve,
+        error: reject
+      });
+    });
+  },
+  getEntries: function () {
+    var Promise = promise.Promise;
+    return new Promise(function (resolve, reject) {
+      $.ajax({
+        url: resourceUrl,
+        method: "GET",
+        dataType: "json",
+        success: resolve,
+        error: reject
+      });
+    });
+  },
+  deleteEntry: function (entry) {
+    var Promise = promise.Promise;
+    return new Promise(function (resolve, reject) {
+      $.ajax({
+        url: resourceUrl + "/" + school._id,
+        method: "DELETE",
+        dataType: "json",
+        success: resolve,
+        error: reject
+      });
+    });
+  }
+}
