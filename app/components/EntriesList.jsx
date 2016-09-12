66586e616031c3994f597f9c42204daa399b5c8c
jsonlitz <jasondotlee@gmail.com>
2016-09-12 10:42:58 -0700

    add delete and add create (front end only)

--- app/components/EntriesList.jsx
+++ app/components/EntriesList.jsx
@@ -1,12 +1,13 @@
 var React = require("react");
 var EntryInfo = require("./EntryInfo.jsx")
+var AddEntry = require("./AddEntry.jsx");
 //we're able to require these constructs even though browsers do not support JSX since we've already set up our Gulp tasks to bundle, transpile and pipe into our main.js in the "dist" directory.
 module.exports = React.createClass({
   render:function(){
     return(
       <div className="row">
         <div className="col-md-6">
-
+          <AddEntry />
         </div>
         <div className="col-md-6">
             {
--- app/components/EntryInfo.jsx
+++ app/components/EntryInfo.jsx
@@ -1,6 +1,12 @@
 var React = require("react");
+var actions = require("../actions/EntryActions");
 
 module.exports = React.createClass({
+  deleteEntry: function(e){
+    e.preventDefault();
+    actions.deleteEntry(this.props.info);
+  },
+  
   render:function(){
     return( // notice these are bootstrap classes and can be embedded directly into the JSX which then gets rendered onto the index.html
       <div className="panel panel-default">
