4fb35ef3aa01d8bc1c39a7c1d49b8d218bd873ab
jsonlitz <jasondotlee@gmail.com>
2016-09-12 11:33:31 -0700

    add body parser mongoose and controllers to server.js

--- server/controllers/entryController.js
+++ server/controllers/entryController.js
@@ -34,4 +34,6 @@ function deleteEntry(req, res) {
   });
 }
 
+
+
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,20 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 var path = require("path");
+var mongoose = require("mongoose");
+
+//controllers
+var entryController = require("./controllers/entryController");
+
+//Express request pipeline
 var app = express();
 app.use(express.static(path.join(__dirname,"../app/dist")));
+app.use(bodyParser.json());
+app.use("/api", entryController);
+
 app.listen(7777,function(){
-  console.log("yo we listening on port", 7777, "fam");
+  console.log("yo, we listening on port", 7777, "fam");
 });
+
+// Connect to mongodb database
+mongoose.connect("mongodb://localhost/retrospec");
